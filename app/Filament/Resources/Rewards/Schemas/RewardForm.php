30892a741eb0303757bb90471f95e8672b46f074
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-21 01:48:39 +0300

    Flatten reward form layout

--- app/Filament/Resources/Rewards/Schemas/RewardForm.php
+++ app/Filament/Resources/Rewards/Schemas/RewardForm.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\Rewards\Schemas;
 use App\Enums\LoyaltyRewardType;
 use App\Enums\RewardStatus;
 use Filament\Forms\Components\DateTimePicker;
-use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
@@ -16,33 +15,30 @@ class RewardForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(2)
             ->components([
-                Section::make('Reward')
-                    ->columns(2)
-                    ->schema([
-                        Select::make('member_id')
-                            ->relationship('member', 'name')
-                            ->required()
-                            ->searchable(),
-                        Select::make('loyalty_rule_id')
-                            ->relationship('loyaltyRule', 'name')
-                            ->nullable()
-                            ->searchable(),
-                        Select::make('type')
-                            ->required()
-                            ->options(self::rewardTypeOptions()),
-                        TextInput::make('value')
-                            ->maxLength(255),
-                        Select::make('status')
-                            ->required()
-                            ->options(self::statusOptions())
-                            ->default(RewardStatus::Pending->value),
-                        DateTimePicker::make('granted_at')
-                            ->disabled()
-                            ->dehydrated(false),
-                        Textarea::make('notes')
-                            ->columnSpanFull(),
-                    ]),
+                Select::make('member_id')
+                    ->relationship('member', 'name')
+                    ->required()
+                    ->searchable(),
+                Select::make('loyalty_rule_id')
+                    ->relationship('loyaltyRule', 'name')
+                    ->nullable()
+                    ->searchable(),
+                Select::make('type')
+                    ->required()
+                    ->options(self::rewardTypeOptions()),
+                TextInput::make('value')
+                    ->maxLength(255),
+                Select::make('status')
+                    ->required()
+                    ->options(self::statusOptions())
+                    ->default(RewardStatus::Pending->value),
+                DateTimePicker::make('granted_at')
+                    ->disabled()
+                    ->dehydrated(false),
+                Textarea::make('notes')
+                    ->columnSpanFull(),
             ]);
     }
 
@@ -60,4 +56,3 @@ class RewardForm
             ->all();
     }
 }
-
