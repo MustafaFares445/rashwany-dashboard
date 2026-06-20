cd26d347e02e56069232af70cf42914ab00678f7
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-21 01:48:30 +0300

    Flatten loyalty rule form layout

--- app/Filament/Resources/LoyaltyRules/Schemas/LoyaltyRuleForm.php
+++ app/Filament/Resources/LoyaltyRules/Schemas/LoyaltyRuleForm.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\LoyaltyRules\Schemas;
 use App\Enums\LoyaltyRewardType;
 use App\Enums\LoyaltyTriggerType;
 use Filament\Forms\Components\KeyValue;
-use Filament\Schemas\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
@@ -16,27 +15,24 @@ class LoyaltyRuleForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(2)
             ->components([
-                Section::make('Rule')
-                    ->columns(2)
-                    ->schema([
-                        TextInput::make('name')
-                            ->required()
-                            ->maxLength(255),
-                        Select::make('trigger_type')
-                            ->required()
-                            ->options(self::triggerTypeOptions()),
-                        Select::make('reward_type')
-                            ->required()
-                            ->options(self::rewardTypeOptions()),
-                        TextInput::make('reward_value')
-                            ->maxLength(255),
-                        Toggle::make('is_active')
-                            ->default(true),
-                        KeyValue::make('condition_json')
-                            ->label('Conditions')
-                            ->columnSpanFull(),
-                    ]),
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Select::make('trigger_type')
+                    ->required()
+                    ->options(self::triggerTypeOptions()),
+                Select::make('reward_type')
+                    ->required()
+                    ->options(self::rewardTypeOptions()),
+                TextInput::make('reward_value')
+                    ->maxLength(255),
+                Toggle::make('is_active')
+                    ->default(true),
+                KeyValue::make('condition_json')
+                    ->label('Conditions')
+                    ->columnSpanFull(),
             ]);
     }
 
@@ -54,4 +50,3 @@ class LoyaltyRuleForm
             ->all();
     }
 }
-
